865b5e1b2fdfb4c2ae10b605acf55110d83510e8
Aditya Mangal <adityamangal0202@gmail.com>
2021-09-10 18:29:38 +0530

    Removed Console statements

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ window.addEventListener('mousedown', mousePosition);
 window.addEventListener('mouseenter', mousePosition);
 
 function mousePosition(e) {
-    console.log('ok')
+//     console.log('ok')
     mousePos.x = e.clientX;
     mousePos.y = e.clientY;
 }
 
 function resize(e) {
-    console.log('ok2')
+//     console.log('ok2')
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
     
 }
 
 function draw(e) {
-    console.log('ok3')
+//     console.log('ok3')
     if (e.buttons !== 1)
         return;
     ctx.beginPath();
